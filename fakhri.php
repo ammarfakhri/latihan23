21706049314c3554798d249bcc7f8db8fba32eb2
Ammar Fakhri <ammarfakhri2@gmail.com>
2018-12-23 07:29:08 +0700

    first commit

---
+++ fakhri.php
@@ -0,0 +1,56 @@
+<html>
+	<head>
+		<title>Ammar Fakhri</title>
+	</head>
+<style>
+body {
+	color: yellow;
+	font-size: 25px;
+	font-family: Arial;
+}
+.VAR {
+	display: inline-block;
+	background-color: green;
+	padding: 50px;
+}
+input {
+	margin-right: 100px;
+	display: inline-block;
+	border-radius: 5px;	
+}
+#judul {
+	color: red;
+	font-size: 30px;
+}
+#submit {
+	font-size: 15px;
+	height: 25px;
+}
+#submit:hover {
+	color: white;
+	background-color: #232323;
+}
+</style>
+<body class="VAR">
+<form method="post">
+<p id="judul"><strong>
+Mencari Angka Yang Besar.</strong>
+</p>
+<label name="varA">Variable A : <label><input type="text" name="varA" id="varA"></input>
+<p>
+<label name="varB">Variable B : <label><input type="text" name="varB" id="varB"></input>
+<p>
+<label name="varc">Variable C : <label><input type="text" name="varC" id="varC"></input>
+<p>
+<input type="submit" name="submit" id="submit" value="Silahkan di Cek :)">
+</form>
+</body>
+</html>
+
+<?php
+$varA=$_POST['varA'];
+$varB=$_POST['varB'];
+$varC=$_POST['varC'];
+
+echo max($varA,$varB,$varC);
+?>
